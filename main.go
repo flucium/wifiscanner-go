dfb1e7be8db92b64bc6d6e69a1535f51b3771e92
(∩´﹏`∩) <14249224+flucium@users.noreply.github.com>
2023-05-29 13:31:11 +0900

    Update main.go

--- main.go
+++ main.go
@@ -3,12 +3,46 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"runtime"
+	"unsafe"
 
 	"golang.org/x/text/encoding/japanese"
 	"golang.org/x/text/transform"
 )
 
 func main() {
-	var out, _ = exec.Command("C://Windows//System32//netsh.exe", "wlan", "show", "networks").Output()
-	fmt.Println(transform.String(japanese.ShiftJIS.NewDecoder(), string(out)))
+	NewScanner().Scan()
+}
+
+// type Mode string
+
+// const (
+// 	SSID  Mode = "ssid"
+// 	BSSID Mode = "Bssid"
+// )
+
+type Scanner struct {
+	program string
+	args    []string
+}
+
+func NewScanner() *Scanner {
+
+	switch runtime.GOOS {
+	case "windows":
+		return &Scanner{
+			program: "C://Windows//System32//netsh.exe",
+			args:    []string{"wlan", "show", "networks"},
+		}
+	default:
+		panic("")
+	}
+
+}
+
+func (s *Scanner) Scan() {
+
+	output, err := exec.Command(s.program, s.args...).Output()
+	fmt.Println(err)
+	fmt.Println(transform.String(japanese.ShiftJIS.NewDecoder(), *(*string)(unsafe.Pointer(&output))))
 }
